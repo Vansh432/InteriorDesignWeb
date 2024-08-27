5a69ee6edf8ea056214f3493775a7661ba6954a4
vansh432 <vanshdeep703@gmail.com>
2024-08-27 18:02:15 +0530

    remove scroll smooth

--- src/App.js
+++ src/App.js
@@ -1,51 +1,33 @@
-import React, { useLayoutEffect, useRef } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import gsap from 'gsap-trial';
-import { ScrollTrigger } from 'gsap-trial/ScrollTrigger';
-import { ScrollSmoother } from 'gsap-trial/ScrollSmoother';
-import Navbar from './Component/Navbar';
-import Home from './Pages/Home';
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Navbar from './Component/Navbar'
+import Home from './Pages/Home'
 import Service from './Pages/Service';
 import Gallery from './Pages/Gallery';
 import Contactus from './Pages/Contactus';
 import Aboutus from './Pages/Aboutus';
-import Topbar from './Component/Topbar';
-import Footer from './Component/Footer';
-import './App.css';
+import Topbar from './Component/Topbar'
+import './App.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Responsive.css';
+import Footer from './Component/Footer'
+function App(){
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-function App() {
-  const main = useRef();
-  const smoother = useRef();
-
-  useLayoutEffect(() => {
-    smoother.current = ScrollSmoother.create({
-      smooth: 1, // seconds it takes to "catch up" to native scroll position
-      effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
-    });
-  }, []);
-
-  return (
-    <>
-      <BrowserRouter>
-        <div id="smooth-wrapper" ref={main}>
-          <div id="smooth-content">
-            <Topbar />
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/service" element={<Service />} />
-              <Route path="/gallery" element={<Gallery />} />
-              <Route path="/contactus" element={<Contactus />} />
-              <Route path="/aboutus" element={<Aboutus />} />
-            </Routes>
-            <Footer />
-          </div>
-        </div>
-      </BrowserRouter>
-    </>
-  );
+  return<>
+  <BrowserRouter>
+  <Topbar/>
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/service" element={<Service/>}/>
+    <Route path="/gallery" element={<Gallery/>}/>
+    <Route path="/contactus" element={<Contactus/>}/>
+    <Route path="/aboutus" element={<Aboutus/>}/>
+   </Routes>
+   <Footer/>
+  </BrowserRouter>
+    
+  </>
 }
 
 export default App;
